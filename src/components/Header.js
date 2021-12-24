7e1f579dad82b8f1de44fd0028227fbca2c46f1b
hubnotch <love2ekene@gmail.com>
2021-12-24 16:57:15 +0100

    styled the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import troll from "../images/troll-face.png"
 export default function Header() {
   return (
     <nav className="header">
-      <img src={troll} alt="logo" />
-      <p>Meme Generator</p>
-      <p>React Course - Project 3</p>
+      <img src={troll} alt="logo" className="logo"/>
+      <p className="logo--title">Meme Generator</p>
+      <p className="project--number">React Course - Project 3</p>
     </nav>
   );
 }
