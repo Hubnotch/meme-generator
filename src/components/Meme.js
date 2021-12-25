e89c3a8231347b072daeca65f7a266ae1ab96fa8
hubnotch <love2ekene@gmail.com>
2021-12-25 05:17:28 +0100

    added memesData as a data from fetched meme data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,13 @@ import React from "react";
 function Meme() {
   return (
     <main className="form--group">
-      <form action="#" className="form">
+      <div className="form">
         <input type="text" placeholder="Text on  top" className="form--input" />
         <input type="text" placeholder="Text below" className="form--input" />
         <button type="submit" className="form--button">
           Get a new meme image 🖼
         </button>
-      </form>
+      </div>
     </main>
   );
 }
