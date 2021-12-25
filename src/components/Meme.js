b7ee59d7818f883d549c5c54bd8335d924f017a2
hubnotch <love2ekene@gmail.com>
2021-12-25 06:09:02 +0100

    getting random image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,21 @@
 import React from "react";
-
+import memesData from "../memesData"
 function Meme() {
+  // console.log(memesData.data.memes[6].url)
+  // console.log(memesData.data.memes.length);
+
+  const getRandomImage = () =>{
+    const memesArray = memesData.data.memes
+    const index = Math.floor(Math.random() * memesArray.length);
+    const image = memesArray[index].url;
+    console.log(image);
+  }
   return (
     <main className="form--group">
       <div className="form">
         <input type="text" placeholder="Text on  top" className="form--input" />
         <input type="text" placeholder="Text below" className="form--input" />
-        <button type="submit" className="form--button">
+        <button onClick={getRandomImage} type="submit" className="form--button">
           Get a new meme image 🖼
         </button>
       </div>
