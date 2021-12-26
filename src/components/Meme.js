33e9140cb2bef6d4e1ca2a35428d030d0e424f7b
hubnotch <love2ekene@gmail.com>
2021-12-26 21:28:43 +0100

    refactored the memeImage state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,28 +1,33 @@
-import React,{useState} from "react";
-import memesData from "../memesData"
-
+import React, { useState } from "react";
+import memesData from "../memesData";
 
 function Meme() {
   // console.log(memesData.data.memes[6].url)
   // console.log(memesData.data.memes.length);
-const [memeImage, setMemeImage] = useState("")
-  const getRandomImage = () =>{
-    const memesArray = memesData.data.memes
+  const [memeImage, setMemeImage] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes;
     const index = Math.floor(Math.random() * memesArray.length);
     const image = memesArray[index].url;
     //console.log(image);
-    setMemeImage( image)
-  }
+    setMemeImage(image);
+  };
   return (
     <main className="form--group">
       <div className="form">
         <input type="text" placeholder="Text on  top" className="form--input" />
         <input type="text" placeholder="Text below" className="form--input" />
-        <button onClick={getRandomImage} type="submit" className="form--button">
+        <button onClick={getMemeImage} type="submit" className="form--button">
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={memeImage} alt={memeImage} className="meme--Image" />
+      <img src={memeImage.randomImage} alt={memeImage.randomImage} className="meme--Image" />
     </main>
   );
 }
